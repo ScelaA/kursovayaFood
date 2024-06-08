f4048044ffccfed5950b288d70fe435873664c2b
ScelaA <ilya.skryabin.03@mail.ru>
2024-06-09 05:12:11 +0700

    POJO(Category)

---
+++ app/src/main/java/com.example.easyfood/data/DAO.kt
@@ -0,0 +1,4 @@
+package com.example.easyfood.data
+
+interface DAO {
+}
---
+++ app/src/main/java/com.example.easyfood/ui/activites/MealDetailesActivity.kt
@@ -0,0 +1,8 @@
+package com.example.easyfood.ui.activites
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MealDetailesActivity : AppCompatActivity() {
+
+
+}
