aa82934dc140450607cf9dd1929f7f640479b7b5
ScelaA <ilya.skryabin.03@mail.ru>
2024-06-09 00:29:34 +0700

    Resources and BuildConfig
    integrating resources into layouts and BuildCongif created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
+
+
+
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -46,4 +50,29 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    // nvigation component
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //viewmodel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.1.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+    //room
+    implementation ("androidx.room:room-runtime:2.3.0")
+    implementation ("androidx.room:room-compiler:2.3.0")
+
+
+    //gif
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.17")
 }
---
+++ app/src/main/java/com/example/easyfood/ui/activites/MealDetailesActivity.kt
@@ -0,0 +1,17 @@
+package com.example.easyfood.ui.activites
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.example.easyfood.R
+import com.google.android.material.snackbar.Snackbar
+
+class MealDetailesActivity : AppCompatActivity() {
+
+}
