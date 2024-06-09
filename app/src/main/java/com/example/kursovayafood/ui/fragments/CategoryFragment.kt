4c93db614c117941b5e7e735156aba7fb9697dde
ScelaA <ilya.skryabin.03@mail.ru>
2024-06-10 00:36:19 +0700

    The final touches
    Adding code for the remaining fragments to display information

--- app/src/main/java/com/example/kursovayafood/ui/fragments/CategoryFragment.kt
+++ app/src/main/java/com/example/kursovayafood/ui/fragments/CategoryFragment.kt
@@ -1,4 +1,77 @@
 package com.example.kursovayafood.ui.fragments
 
-class CategoryFragment {
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.kursovayafood.R
+import com.example.kursovayafood.adapters.CategoriesRecyclerAdapter
+import com.example.kursovayafood.data.pojo.Category
+import com.example.kursovayafood.databinding.FragmentCategoryBinding
+import com.example.kursovayafood.mvvm.CategoryMVVM
+import com.example.kursovayafood.ui.activites.MealActivity
+import com.example.kursovayafood.ui.fragments.HomeFragment
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.CATEGORY_NAME
+
+
+class CategoryFragment : Fragment(R.layout.fragment_category) {
+    private lateinit var binding:FragmentCategoryBinding
+    private lateinit var myAdapter:CategoriesRecyclerAdapter
+    private lateinit var categoryMvvm:CategoryMVVM
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        myAdapter = CategoriesRecyclerAdapter()
+        categoryMvvm = ViewModelProviders.of(this)[CategoryMVVM::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCategoryBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        prepareRecyclerView()
+        observeCategories()
+        onCategoryClick()
+    }
+
+    private fun onCategoryClick() {
+        myAdapter.onItemClicked(object : CategoriesRecyclerAdapter.OnItemCategoryClicked{
+            override fun onClickListener(category: Category) {
+                val intent = Intent(context, MealActivity::class.java)
+                intent.putExtra(CATEGORY_NAME,category.strCategory)
+                startActivity(intent)
+            }
+        })
+    }
+
+    private fun observeCategories() {
+        categoryMvvm.observeCategories().observe(viewLifecycleOwner,object : Observer<List<Category>>{
+            override fun onChanged(t: List<Category>?) {
+                myAdapter.setCategoryList(t!!)
+            }
+
+        })
+    }
+
+    private fun prepareRecyclerView() {
+        binding.favoriteRecyclerView.apply {
+            adapter = myAdapter
+            layoutManager = GridLayoutManager(context,3,GridLayoutManager.VERTICAL,false)
+        }
+    }
+
+
 }
