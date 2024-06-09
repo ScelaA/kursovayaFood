da488ef3b36cd732a7286386ec0f6a5d73dded56
ScelaA <ilya.skryabin.03@mail.ru>
2024-06-09 19:40:48 +0700

    MealActivity and Bottom

--- app/src/main/java/com/example/kursovayafood/ui/MealBottomDialog.kt
+++ app/src/main/java/com/example/kursovayafood/ui/MealBottomDialog.kt
@@ -11,8 +11,70 @@ import com.bumptech.glide.Glide
 import com.example.kursovayafood.R
 import com.example.kursovayafood.ui.activites.MealDetailesActivity
 
+import com.example.kursovayafood.ui.fragments.HomeFragment
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.CATEGORY_NAME
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_AREA
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_ID
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_NAME
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_STR
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_THUMB
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MealBottomDialog() : BottomSheetDialogFragment() {
+    private var mealName = ""
+    private var mealId =""
+    private var mealImg = ""
+    private var mealCountry = ""
+    private var mealCategory = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL,R.style.AppBottomSheetDialogTheme)
+        val b = arguments
+        mealName = b!!.getString(MEAL_NAME).toString()
+        mealId =b!!.getString(MEAL_ID).toString()
+        mealImg =b!!.getString(MEAL_THUMB).toString()
+        mealCategory =b!!.getString(CATEGORY_NAME).toString()
+        mealCountry =b!!.getString(MEAL_AREA).toString()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = LayoutInflater.from(context).inflate(R.layout.bottom_sheet_dialog, container, false)
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prepareView(view)
+
+        view.setOnClickListener {
+            val intent = Intent(context, MealDetailesActivity::class.java)
+            intent.putExtra(MEAL_ID,mealId)
+            intent.putExtra(MEAL_STR,mealName)
+            intent.putExtra(MEAL_THUMB,mealImg)
+            startActivity(intent)
+        }
+
+    }
+
+    fun prepareView(view:View){
+        val tvMealName = view.findViewById<TextView>(R.id.tv_meal_name_in_btmsheet)
+        val tvMealCategory = view.findViewById<TextView>(R.id.tv_meal_category)
+        val tvMealCountry = view.findViewById<TextView>(R.id.tv_meal_country)
+        val imgMeal = view.findViewById<ImageView>(R.id.img_category)
+
+        Glide.with(view)
+            .load(mealImg)
+            .into(imgMeal)
+        tvMealName.text = mealName
+        tvMealCategory.text = mealCategory
+        tvMealCountry.text = mealCountry
+    }
+
 
 }
--- app/src/main/java/com/example/kursovayafood/ui/activites/MealActivity.kt
+++ app/src/main/java/com/example/kursovayafood/ui/activites/MealActivity.kt
@@ -10,10 +10,67 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.kursovayafood.R
+
 import com.example.kursovayafood.data.pojo.Meal
 import com.example.kursovayafood.databinding.ActivityCategoriesBinding
-
+import com.example.kursovayafood.mvvm.MealActivityMVVM
+import com.example.kursovayafood.ui.fragments.HomeFragment
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.CATEGORY_NAME
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_ID
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_STR
+import com.example.kursovayafood.ui.fragments.HomeFragment.Companion.MEAL_THUMB
 
 class MealActivity : AppCompatActivity() {
+    private lateinit var mealActivityMvvm: MealActivityMVVM
+    private lateinit var binding: ActivityCategoriesBinding
+    private var categoryNme = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCategoriesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mealActivityMvvm = ViewModelProviders.of(this)[MealActivityMVVM::class.java]
+        startLoading()
+        prepareRecyclerView()
+        mealActivityMvvm.getMealsByCategory(getCategory())
+        mealActivityMvvm.observeMeal().observe(this, object : Observer<List<Meal>> {
+            override fun onChanged(t: List<Meal>?) {
+                if(t==null){
+                    hideLoading()
+                    Toast.makeText(applicationContext, "No meals in this category", Toast.LENGTH_SHORT).show()
+                    onBackPressed()
+                }else {
+                    binding.tvCategoryCount.text = categoryNme + " : " + t.size.toString()
+                    hideLoading()
+                }
+            }
+        })
 
+
+    }
+
+    private fun hideLoading() {
+        binding.apply {
+            loadingGifMeals.visibility = View.INVISIBLE
+            mealRoot.setBackgroundColor(ContextCompat.getColor(applicationContext,R.color.white))
+        }    }
+
+    private fun startLoading() {
+        binding.apply {
+            loadingGifMeals.visibility = View.VISIBLE
+            mealRoot.setBackgroundColor(ContextCompat.getColor(applicationContext,R.color.g_loading))
+        }
+    }
+
+    private fun getCategory(): String {
+        val tempIntent = intent
+        val x = intent.getStringExtra(CATEGORY_NAME)!!
+        categoryNme = x
+        return x
+    }
+
+    private fun prepareRecyclerView() {
+        binding.mealRecyclerview.apply {
+            layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+        }
+    }
 }
